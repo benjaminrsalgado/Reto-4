b7b6c282fb0fea7b6442dab77f1734597cedd2c9
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-03-27 13:29:39 -0600

    Initial Commit

---
+++ Reto 4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reto 4
+//
+//  Created by Benjamin Rojo on 27/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reto 4/Reto_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Reto_4App.swift
+//  Reto 4
+//
+//  Created by Benjamin Rojo on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Reto_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
